1ad793b64b53b052787969f61f77ac29579b2b66
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-20 20:43:46 +0000

    Updated widget classes
    git-svn-id: http://plugins.svn.wordpress.org/mtphr-widgets/trunk@1226549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/collapse.php
+++ includes/collapse.php
@@ -1,173 +1,128 @@
 <?php
 
-/* Register the widget - @since 2.0.3 */
-add_action( 'widgets_init', 'mtphr_collapse_widget_init' );
-
-/**
- * Register the widget
- *
- * @since 2.0.3
- */
-function mtphr_collapse_widget_init() {
-	register_widget( 'mtphr_collapse_widget' );
-}
-
-
-
-
 /**
  * Create a class for the widget
  *
- * @since 2.0.3
+ * @since 2.2
  */
 class mtphr_collapse_widget extends WP_Widget {
 	
-/**
- * Widget setup
- *
- * @since 2.0.3
- */
-function mtphr_collapse_widget() {
-	
-	// Widget settings
-	$widget_ops = array(
-		'classname' => 'mtphr-collapse-widget',
-		'description' => __('Displays collapsible content.', 'mtphr-widgets')
-	);
-
-	// Widget control settings
-	$control_ops = array(
-		'id_base' => 'mtphr-collapse',
-		'width' => 400
-	);
-
-	// Create the widget
-	$this->WP_Widget( 'mtphr-collapse', __('Metaphor Collapse', 'mtphr-widgets'), $widget_ops, $control_ops );
-}
-
-/**
- * Display the widget
- *
- * @since 2.0.6
- */
-function widget( $args, $instance ) {
-	
-	extract( $args );
-
-	// User-selected settings	
-	$title = $instance['title'];
-	$title = apply_filters( 'widget_title', $title );
-	
-	$widget_id = ( isset($args['widget_id']) ) ? $args['widget_id'] : -1;
-	$collapse_info = apply_filters( 'mtphr_widgets_collapse_info', $instance['collapse_info'], $widget_id );
-	
-	// Before widget (defined by themes)
-	echo $before_widget;
-	
-	// Title of widget (before and after defined by themes)
-	if ( $title ) {
-		echo $before_title . $title . $after_title;
+	/** Constructor */
+	function __construct() {
+		parent::__construct( 'mtphr-collapse', __('Metaphor Collapse', 'mtphr-widgets'), array( 'description' => __('Displays collapsible content.', 'mtphr-widgets') ) );
 	}
 	
-	if( isset($collapse_info[0]) ) {
-		foreach( $collapse_info as $info ) {
-			if( $info['title'] != '' ) {
-				$active = ( isset($info['open']) && $info['open'] == 'on' ) ? ' active' : '';
-				$style = ( isset($info['open']) && $info['open'] == 'on' ) ? ' style="display:block;"' : '';
-				echo '<div class="mtphr-collapse-widget-block">';
-					echo '<p class="mtphr-collapse-widget-heading'.$active.'">';
-						$toggle = apply_filters( 'mtphr_collapse_widget_toggle', '<span class="mtphr-collapse-widget-toggle"><span class="mtphr-toggle-expand"><i class="metaphor-widgets-ico-plus-square"></i></span><span class="mtphr-toggle-collapse"><i class="metaphor-widgets-ico-minus-square"></i></span></span>' );
-						echo '<a href="#">'.$toggle.sanitize_text_field($info['title']).'</a>';
-				echo '</p>';
-				echo '<p class="mtphr-collapse-widget-description"'.$style.'>'.make_clickable($info['description']).'</p></div>';
-			}	
+	/** @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+		
+		extract( $args );
+	
+		// User-selected settings	
+		$title = $instance['title'];
+		$title = apply_filters( 'widget_title', $title );
+		
+		$widget_id = ( isset($args['widget_id']) ) ? $args['widget_id'] : -1;
+		$collapse_info = apply_filters( 'mtphr_widgets_collapse_info', $instance['collapse_info'], $widget_id );
+		
+		// Before widget (defined by themes)
+		echo $before_widget;
+		
+		// Title of widget (before and after defined by themes)
+		if ( $title ) {
+			echo $before_title . $title . $after_title;
 		}
-	}
-	
-	// After widget (defined by themes)
-	echo $after_widget;
-}
-
-/**
- * Update the widget
- *
- * @since 2.0.3
- */
-function update( $new_instance, $old_instance ) {
-	
-	$instance = $old_instance;
-
-	// Strip tags (if needed) and update the widget settings
-	$instance['title'] = sanitize_text_field( $new_instance['title'] );
-	$instance['collapse_info'] = $new_instance['collapse_info'];
-	$instance['advanced'] = $new_instance['advanced'];
-
-	return $instance;
-}
-
-/**
- * Widget settings
- *
- * @since 2.1.15
- */
-function form( $instance ) {
-
-	// Set up some default widget settings
-	$defaults = array(
-		'title' => __('Information', 'mtphr-widgets'),
-		'collapse_info' => array(
-			array(
-				'title' => __('Add title here...', 'mtphr-widgets'),
-				'description' => __('Add a description here...', 'mtphr-widgets')
-			)
-		),
-		'advanced' => ''
-	);
-	
-	$instance = wp_parse_args( (array) $instance, $defaults ); ?>
-	
-  <!-- Widget Title: Text Input -->
-	<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'mtphr-widgets' ); ?></label>
-		<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:97%;" />
-	</p>
-	
-	<?php echo metaphor_widgets_collapse_setup( $this->get_field_name('collapse_info'), $instance['collapse_info'] ); ?>
-	
-	<!-- Advanced: Checkbox -->
-	<p class="mtphr-widget-advanced">
-		<input class="checkbox" type="checkbox" <?php checked( $instance['advanced'], 'on' ); ?> id="<?php echo $this->get_field_id( 'advanced' ); ?>" name="<?php echo $this->get_field_name( 'advanced' ); ?>" />
-		<label for="<?php echo $this->get_field_id( 'advanced' ); ?>"><?php _e( 'Show Advanced Info', 'mtphr-widgets' ); ?></label>
-	</p>
-	
-	<!-- Widget ID: Text -->
-	<p class="mtphr-widget-id">
-		<label for="<?php echo $this->get_field_id( 'widget_id' ); ?>"><?php _e( 'Widget ID:', 'mtphr-widgets' ); ?></label>
-		<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'widget_id' ); ?>" name="<?php echo $this->get_field_name( 'widget_id' ); ?>" value="<?php echo substr( $this->get_field_id(''), 0, -1 ); ?>" style="width:97%;" disabled />
-	</p>
-	
-	<!-- Shortcode -->
-	<span class="mtphr-widget-shortcode">
-		<label><?php _e( 'Shortcode:', 'mtphr-widgets' ); ?></label>
-		<?php
-		$shortcode = '[mtphr_collapse_widget';
-		$shortcode .= ( $instance['title'] != '' ) ? ' title="'.$instance['title'].'"' : '';
-		$shortcode .= ']';
-		if( isset($instance['collapse_info'][0]) ) {
-			$all_info = '';
-			foreach( $instance['collapse_info'] as $info ) {
-				$all_info .= sanitize_text_field($info['title']).'***'.esc_attr(nl2br($info['description'])).':::';
+		
+		if( isset($collapse_info[0]) ) {
+			foreach( $collapse_info as $info ) {
+				if( $info['title'] != '' ) {
+					$active = ( isset($info['open']) && $info['open'] == 'on' ) ? ' active' : '';
+					$style = ( isset($info['open']) && $info['open'] == 'on' ) ? ' style="display:block;"' : '';
+					echo '<div class="mtphr-collapse-widget-block">';
+						echo '<p class="mtphr-collapse-widget-heading'.$active.'">';
+							$toggle = apply_filters( 'mtphr_collapse_widget_toggle', '<span class="mtphr-collapse-widget-toggle"><span class="mtphr-toggle-expand"><i class="metaphor-widgets-ico-plus-square"></i></span><span class="mtphr-toggle-collapse"><i class="metaphor-widgets-ico-minus-square"></i></span></span>' );
+							echo '<a href="#">'.$toggle.sanitize_text_field($info['title']).'</a>';
+					echo '</p>';
+					echo '<p class="mtphr-collapse-widget-description"'.$style.'>'.make_clickable($info['description']).'</p></div>';
+				}	
 			}
-			$all_info = substr( $all_info, 0, -3 );
-			$shortcode .= $all_info.'"';
 		}
-		$shortcode .= '[/mtphr_collapse_widget]';
-		?>
-		<pre class="mtphr-widgets-code"><p><?php echo $shortcode; ?></p></pre>
-	</span>
-  	
-	<?php
-}
+		
+		// After widget (defined by themes)
+		echo $after_widget;
+	}
+	
+	/** @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+		
+		$instance = $old_instance;
+	
+		// Strip tags (if needed) and update the widget settings
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['collapse_info'] = $new_instance['collapse_info'];
+		$instance['advanced'] = $new_instance['advanced'];
+	
+		return $instance;
+	}
+	
+	/** @see WP_Widget::form */
+	function form( $instance ) {
+	
+		// Set up some default widget settings
+		$defaults = array(
+			'title' => __('Information', 'mtphr-widgets'),
+			'collapse_info' => array(
+				array(
+					'title' => __('Add title here...', 'mtphr-widgets'),
+					'description' => __('Add a description here...', 'mtphr-widgets')
+				)
+			),
+			'advanced' => ''
+		);
+		
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+		
+	  <!-- Widget Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'mtphr-widgets' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:97%;" />
+		</p>
+		
+		<?php echo metaphor_widgets_collapse_setup( $this->get_field_name('collapse_info'), $instance['collapse_info'] ); ?>
+		
+		<!-- Advanced: Checkbox -->
+		<p class="mtphr-widget-advanced">
+			<input class="checkbox" type="checkbox" <?php checked( $instance['advanced'], 'on' ); ?> id="<?php echo $this->get_field_id( 'advanced' ); ?>" name="<?php echo $this->get_field_name( 'advanced' ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'advanced' ); ?>"><?php _e( 'Show Advanced Info', 'mtphr-widgets' ); ?></label>
+		</p>
+		
+		<!-- Widget ID: Text -->
+		<p class="mtphr-widget-id">
+			<label for="<?php echo $this->get_field_id( 'widget_id' ); ?>"><?php _e( 'Widget ID:', 'mtphr-widgets' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'widget_id' ); ?>" name="<?php echo $this->get_field_name( 'widget_id' ); ?>" value="<?php echo substr( $this->get_field_id(''), 0, -1 ); ?>" style="width:97%;" disabled />
+		</p>
+		
+		<!-- Shortcode -->
+		<span class="mtphr-widget-shortcode">
+			<label><?php _e( 'Shortcode:', 'mtphr-widgets' ); ?></label>
+			<?php
+			$shortcode = '[mtphr_collapse_widget';
+			$shortcode .= ( $instance['title'] != '' ) ? ' title="'.$instance['title'].'"' : '';
+			$shortcode .= ']';
+			if( isset($instance['collapse_info'][0]) ) {
+				$all_info = '';
+				foreach( $instance['collapse_info'] as $info ) {
+					$all_info .= sanitize_text_field($info['title']).'***'.esc_attr(nl2br($info['description'])).':::';
+				}
+				$all_info = substr( $all_info, 0, -3 );
+				$shortcode .= $all_info.'"';
+			}
+			$shortcode .= '[/mtphr_collapse_widget]';
+			?>
+			<pre class="mtphr-widgets-code"><p><?php echo $shortcode; ?></p></pre>
+		</span>
+	  	
+		<?php
+	}
 }
 
 
@@ -231,3 +186,12 @@ function metaphor_widgets_collapse_row( $name, $data=false ) {
 }
 }
 
+
+/* --------------------------------------------------------- */
+/* !Register the widget - 2.2 */
+/* --------------------------------------------------------- */
+
+function mtphr_collapse_widget_init() {
+	register_widget( 'mtphr_collapse_widget' );
+}
+add_action( 'widgets_init', 'mtphr_collapse_widget_init' );
--- includes/twitter.php
+++ includes/twitter.php
@@ -1,205 +1,161 @@
 <?php
 
-/* Register the widget - @since 1.0 */
-add_action( 'widgets_init', 'mtphr_twitter_widget_init' );
-
-/**
- * Register the widget
- *
- * @since 1.0
- */
-function mtphr_twitter_widget_init() {
-	register_widget( 'mtphr_twitter_widget' );
-}
-
-
-
-
 /**
  * Create a class for the widget
  *
- * @since 1.0
+ * @since 2.2
  */
 class mtphr_twitter_widget extends WP_Widget {
-
-/**
- * Widget setup
- *
- * @since 1.0
- */
-function mtphr_twitter_widget() {
-
-	// Widget settings
-	$widget_ops = array(
-		'classname' => 'mtphr-twitter-widget',
-		'description' => __('Displays a users latest twitter comments.', 'mtphr-widgets')
-	);
-
-	// Widget control settings
-	$control_ops = array(
-		'id_base' => 'mtphr-twitter'
-	);
-
-	// Create the widget
-	$this->WP_Widget( 'mtphr-twitter', __('Metaphor Twitter Feed', 'mtphr-widgets'), $widget_ops, $control_ops );
-}
-
-/**
- * Display the widget
- *
- * @since 2.1.8
- */
-function widget( $args, $instance ) {
-
-	extract( $args );
-
-	// User-selected settings
-	$title = $instance['title'];
-	$title = apply_filters( 'widget_title', $title );
-
-	$widget_id = ( isset($args['widget_id']) ) ? $args['widget_id'] : -1;
-	$twitter_name = apply_filters( 'mtphr_widgets_twitter_name', sanitize_text_field( $instance['twitter_name'] ), $widget_id );
-	$widget_limit = apply_filters( 'mtphr_widgets_twitter_limit', intval( $instance['widget_limit'] ), $widget_id );
-	$twitter_image = apply_filters( 'mtphr_widgets_twitter_image', isset( $instance['widget_image'] ), $widget_id );
-	$twitter_avatar = apply_filters( 'mtphr_widgets_twitter_avatar', isset( $instance['widget_avatar'] ), $widget_id );
-	$new_window = ( !isset($instance['new_window_default']) ) ? 'on' : isset( $instance['new_window'] );
-
-	if ( $widget_limit == '' || $widget_limit == 0 ) {
-		$widget_limit = 3;
+	
+	/** Constructor */
+	function __construct() {
+		parent::__construct( 'mtphr-twitter', __('Metaphor Twitter Feed', 'mtphr-widgets'), array( 'description' => __('Displays a users latest twitter comments.', 'mtphr-widgets') ) );
 	}
-
-	// Before widget (defined by themes)
-	echo $before_widget;
-
-	// Title of widget (before and after defined by themes)
-	if ( $title ) {
-		echo $before_title . $title . $after_title;
+		
+	/** @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+	
+		extract( $args );
+	
+		// User-selected settings
+		$title = $instance['title'];
+		$title = apply_filters( 'widget_title', $title );
+	
+		$widget_id = ( isset($args['widget_id']) ) ? $args['widget_id'] : -1;
+		$twitter_name = apply_filters( 'mtphr_widgets_twitter_name', sanitize_text_field( $instance['twitter_name'] ), $widget_id );
+		$widget_limit = apply_filters( 'mtphr_widgets_twitter_limit', intval( $instance['widget_limit'] ), $widget_id );
+		$twitter_image = apply_filters( 'mtphr_widgets_twitter_image', isset( $instance['widget_image'] ), $widget_id );
+		$twitter_avatar = apply_filters( 'mtphr_widgets_twitter_avatar', isset( $instance['widget_avatar'] ), $widget_id );
+		$new_window = ( !isset($instance['new_window_default']) ) ? 'on' : isset( $instance['new_window'] );
+	
+		if ( $widget_limit == '' || $widget_limit == 0 ) {
+			$widget_limit = 3;
+		}
+	
+		// Before widget (defined by themes)
+		echo $before_widget;
+	
+		// Title of widget (before and after defined by themes)
+		if ( $title ) {
+			echo $before_title . $title . $after_title;
+		}
+	
+		if( !mtphr_widgets_check_twitter_access() ) {
+			echo __( 'Twitter feed must be authorized.', 'mtphr-widgets' );
+		} else {
+			// Display the twitter feed
+			mtphr_twitter_widget_feed( $twitter_name, $widget_limit, $twitter_image, $twitter_avatar, $new_window );
+		}
+	
+		// After widget (defined by themes)
+		echo $after_widget;
 	}
-
-	if( !mtphr_widgets_check_twitter_access() ) {
-		echo __( 'Twitter feed must be authorized.', 'mtphr-widgets' );
-	} else {
-		// Display the twitter feed
-		mtphr_twitter_widget_feed( $twitter_name, $widget_limit, $twitter_image, $twitter_avatar, $new_window );
+	
+	/** @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+	
+		$instance = $old_instance;
+	
+		// Strip tags (if needed) and update the widget settings
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['twitter_name'] = sanitize_text_field( strip_tags($new_instance['twitter_name']) );
+		$instance['widget_limit'] = intval( $new_instance['widget_limit'] );
+		$instance['widget_image'] = $new_instance['widget_image'];
+		$instance['widget_avatar'] = $new_instance['widget_avatar'];
+		$instance['new_window'] = $new_instance['new_window'];
+		$instance['new_window_default'] = $new_instance['new_window_default'];
+		$instance['advanced'] = $new_instance['advanced'];
+	
+		return $instance;
 	}
-
-	// After widget (defined by themes)
-	echo $after_widget;
-}
-
-/**
- * Update the widget
- *
- * @since 2.1.8
- */
-function update( $new_instance, $old_instance ) {
-
-	$instance = $old_instance;
-
-	// Strip tags (if needed) and update the widget settings
-	$instance['title'] = sanitize_text_field( $new_instance['title'] );
-	$instance['twitter_name'] = sanitize_text_field( strip_tags($new_instance['twitter_name']) );
-	$instance['widget_limit'] = intval( $new_instance['widget_limit'] );
-	$instance['widget_image'] = $new_instance['widget_image'];
-	$instance['widget_avatar'] = $new_instance['widget_avatar'];
-	$instance['new_window'] = $new_instance['new_window'];
-	$instance['new_window_default'] = $new_instance['new_window_default'];
-	$instance['advanced'] = $new_instance['advanced'];
-
-	return $instance;
-}
-
-/**
- * Widget settings
- *
- * @since 2.1.8
- */
-function form( $instance ) {
-
-	// Set up some default widget settings
-	$defaults = array(
-		'title' => __('Recent Tweets', 'mtphr-widgets'),
-		'twitter_name' => '',
-		'widget_limit' => 3,
-		'widget_image' => '',
-		'widget_avatar' => '',
-		'new_window' => '',
-		'new_window_default' => '',
-		'advanced' => ''
-	);
-
-	$instance = wp_parse_args( (array) $instance, $defaults );
-	$instance['new_window'] = ( $instance['new_window_default'] == '' ) ? 'on' : $instance['new_window'];
-
-	if( !mtphr_widgets_check_twitter_access() ) {
-		$link = admin_url().'plugins.php?page=mtphr_widgets_twitter_settings'; ?>
-
-    <div>
-       <p><?php _e('You must authorize <strong>Metaphor Widgets</strong> access through Twitter before you can display any feeds.', 'mtphr-widgets'); ?><br/><br/><?php printf( __('<a href="%s"><strong>Click here</strong></a> for instructions on creating an app and granting acces to <strong>Metaphor Widgets</strong>.', 'mtphr-widgets'), $link ); ?></p>
-    </div>
-
-  <?php } else { ?>
-
-	  <!-- Widget Title: Text Input -->
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'mtphr-widgets' ); ?></label>
-			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:97%;" />
-		</p>
-
-	  <!-- Twitter Username: Text Input -->
-		<p>
-			<label for="<?php echo $this->get_field_id( 'twitter_name' ); ?>"><?php _e( 'Twitter Username:', 'mtphr-widgets' ); ?></label>
-			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'twitter_name' ); ?>" name="<?php echo $this->get_field_name( 'twitter_name' ); ?>" value="<?php echo $instance['twitter_name']; ?>" style="width:97%;" />
-		</p>
-
-	  <!-- Widget Limit: Text Input -->
-		<p>
-			<label for="<?php echo $this->get_field_id( 'widget_limit' ); ?>"><?php _e( 'Number of Tweets:', 'mtphr-widgets' ); ?></label><br/>
-			<input class="widefat" type="number" id="<?php echo $this->get_field_id( 'widget_limit' ); ?>" name="<?php echo $this->get_field_name( 'widget_limit' ); ?>" value="<?php echo $instance['widget_limit']; ?>" style="width:50px;" />
-
-			<label style="margin-left:10px;"><input class="checkbox" type="checkbox" value="on" <?php checked( $instance['new_window'], 'on' ); ?> name="<?php echo $this->get_field_name( 'new_window' ); ?>" /> <?php _e('Open links in window/tab', 'mtphr-widgets'); ?></label>
-			<input type="hidden" value="on" name="<?php echo $this->get_field_name( 'new_window_default' ); ?>" />
-		</p>
-
-		<!-- Display Widget Image: Checkbox -->
-		<p>
-			<input class="checkbox" type="checkbox" <?php checked( $instance['widget_image'], 'on' ); ?> id="<?php echo $this->get_field_id( 'widget_image' ); ?>" name="<?php echo $this->get_field_name( 'widget_image' ); ?>" />
-			<label for="<?php echo $this->get_field_id( 'widget_image' ); ?>"><?php _e( 'Show Icon?', 'mtphr-widgets' ); ?></label>
-			&nbsp;
-			<!-- Use Avatar: Checkbox -->
-			<input class="checkbox" type="checkbox" <?php checked( $instance['widget_avatar'], 'on' ); ?> id="<?php echo $this->get_field_id( 'widget_avatar' ); ?>" name="<?php echo $this->get_field_name( 'widget_avatar' ); ?>" />
-			<label for="<?php echo $this->get_field_id( 'widget_avatar' ); ?>"><?php _e( 'Use Avatar?', 'mtphr-widgets' ); ?></label>
-		</p>
-
-		<!-- Advanced: Checkbox -->
-		<p class="mtphr-widget-advanced">
-			<input class="checkbox" type="checkbox" <?php checked( $instance['advanced'], 'on' ); ?> id="<?php echo $this->get_field_id( 'advanced' ); ?>" name="<?php echo $this->get_field_name( 'advanced' ); ?>" />
-			<label for="<?php echo $this->get_field_id( 'advanced' ); ?>"><?php _e( 'Show Advanced Info', 'mtphr-widgets' ); ?></label>
-		</p>
-
-		<!-- Widget ID: Text -->
-		<p class="mtphr-widget-id">
-			<label for="<?php echo $this->get_field_id( 'widget_id' ); ?>"><?php _e( 'Widget ID:', 'mtphr-widgets' ); ?></label>
-			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'widget_id' ); ?>" name="<?php echo $this->get_field_name( 'widget_id' ); ?>" value="<?php echo substr( $this->get_field_id(''), 0, -1 ); ?>" style="width:97%;" disabled />
-		</p>
-
-		<!-- Shortcode -->
-		<span class="mtphr-widget-shortcode">
-			<label><?php _e( 'Shortcode:', 'mtphr-widgets' ); ?></label>
-			<?php
-			$shortcode = '[mtphr_twitter_widget';
-			$shortcode .= ( $instance['title'] != '' ) ? ' title="'.$instance['title'].'"' : '';
-			$shortcode .= ( $instance['twitter_name'] != '' ) ? ' twitter_name="'.$instance['twitter_name'].'"' : '';
-			$shortcode .= ( $instance['widget_limit'] != '' ) ? ' limit="'.$instance['widget_limit'].'"' : '';
-			$shortcode .= ( $instance['widget_image'] != '' ) ? ' image="'.$instance['widget_image'].'"' : '';
-			$shortcode .= ( $instance['widget_avatar'] != '' ) ? ' avatar="'.$instance['widget_avatar'].'"' : '';
-			$shortcode .= ']';
-			?>
-			<pre class="mtphr-widgets-code"><p><?php echo $shortcode; ?></p></pre>
-		</span>
-
-	<?php } // End else statement
-}
+	
+	/** @see WP_Widget::form */
+	function form( $instance ) {
+	
+		// Set up some default widget settings
+		$defaults = array(
+			'title' => __('Recent Tweets', 'mtphr-widgets'),
+			'twitter_name' => '',
+			'widget_limit' => 3,
+			'widget_image' => '',
+			'widget_avatar' => '',
+			'new_window' => '',
+			'new_window_default' => '',
+			'advanced' => ''
+		);
+	
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$instance['new_window'] = ( $instance['new_window_default'] == '' ) ? 'on' : $instance['new_window'];
+	
+		if( !mtphr_widgets_check_twitter_access() ) {
+			$link = admin_url().'plugins.php?page=mtphr_widgets_twitter_settings'; ?>
+	
+	    <div>
+	       <p><?php _e('You must authorize <strong>Metaphor Widgets</strong> access through Twitter before you can display any feeds.', 'mtphr-widgets'); ?><br/><br/><?php printf( __('<a href="%s"><strong>Click here</strong></a> for instructions on creating an app and granting acces to <strong>Metaphor Widgets</strong>.', 'mtphr-widgets'), $link ); ?></p>
+	    </div>
+	
+	  <?php } else { ?>
+	
+		  <!-- Widget Title: Text Input -->
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'mtphr-widgets' ); ?></label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:97%;" />
+			</p>
+	
+		  <!-- Twitter Username: Text Input -->
+			<p>
+				<label for="<?php echo $this->get_field_id( 'twitter_name' ); ?>"><?php _e( 'Twitter Username:', 'mtphr-widgets' ); ?></label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'twitter_name' ); ?>" name="<?php echo $this->get_field_name( 'twitter_name' ); ?>" value="<?php echo $instance['twitter_name']; ?>" style="width:97%;" />
+			</p>
+	
+		  <!-- Widget Limit: Text Input -->
+			<p>
+				<label for="<?php echo $this->get_field_id( 'widget_limit' ); ?>"><?php _e( 'Number of Tweets:', 'mtphr-widgets' ); ?></label><br/>
+				<input class="widefat" type="number" id="<?php echo $this->get_field_id( 'widget_limit' ); ?>" name="<?php echo $this->get_field_name( 'widget_limit' ); ?>" value="<?php echo $instance['widget_limit']; ?>" style="width:50px;" />
+	
+				<label style="margin-left:10px;"><input class="checkbox" type="checkbox" value="on" <?php checked( $instance['new_window'], 'on' ); ?> name="<?php echo $this->get_field_name( 'new_window' ); ?>" /> <?php _e('Open links in window/tab', 'mtphr-widgets'); ?></label>
+				<input type="hidden" value="on" name="<?php echo $this->get_field_name( 'new_window_default' ); ?>" />
+			</p>
+	
+			<!-- Display Widget Image: Checkbox -->
+			<p>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['widget_image'], 'on' ); ?> id="<?php echo $this->get_field_id( 'widget_image' ); ?>" name="<?php echo $this->get_field_name( 'widget_image' ); ?>" />
+				<label for="<?php echo $this->get_field_id( 'widget_image' ); ?>"><?php _e( 'Show Icon?', 'mtphr-widgets' ); ?></label>
+				&nbsp;
+				<!-- Use Avatar: Checkbox -->
+				<input class="checkbox" type="checkbox" <?php checked( $instance['widget_avatar'], 'on' ); ?> id="<?php echo $this->get_field_id( 'widget_avatar' ); ?>" name="<?php echo $this->get_field_name( 'widget_avatar' ); ?>" />
+				<label for="<?php echo $this->get_field_id( 'widget_avatar' ); ?>"><?php _e( 'Use Avatar?', 'mtphr-widgets' ); ?></label>
+			</p>
+	
+			<!-- Advanced: Checkbox -->
+			<p class="mtphr-widget-advanced">
+				<input class="checkbox" type="checkbox" <?php checked( $instance['advanced'], 'on' ); ?> id="<?php echo $this->get_field_id( 'advanced' ); ?>" name="<?php echo $this->get_field_name( 'advanced' ); ?>" />
+				<label for="<?php echo $this->get_field_id( 'advanced' ); ?>"><?php _e( 'Show Advanced Info', 'mtphr-widgets' ); ?></label>
+			</p>
+	
+			<!-- Widget ID: Text -->
+			<p class="mtphr-widget-id">
+				<label for="<?php echo $this->get_field_id( 'widget_id' ); ?>"><?php _e( 'Widget ID:', 'mtphr-widgets' ); ?></label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'widget_id' ); ?>" name="<?php echo $this->get_field_name( 'widget_id' ); ?>" value="<?php echo substr( $this->get_field_id(''), 0, -1 ); ?>" style="width:97%;" disabled />
+			</p>
+	
+			<!-- Shortcode -->
+			<span class="mtphr-widget-shortcode">
+				<label><?php _e( 'Shortcode:', 'mtphr-widgets' ); ?></label>
+				<?php
+				$shortcode = '[mtphr_twitter_widget';
+				$shortcode .= ( $instance['title'] != '' ) ? ' title="'.$instance['title'].'"' : '';
+				$shortcode .= ( $instance['twitter_name'] != '' ) ? ' twitter_name="'.$instance['twitter_name'].'"' : '';
+				$shortcode .= ( $instance['widget_limit'] != '' ) ? ' limit="'.$instance['widget_limit'].'"' : '';
+				$shortcode .= ( $instance['widget_image'] != '' ) ? ' image="'.$instance['widget_image'].'"' : '';
+				$shortcode .= ( $instance['widget_avatar'] != '' ) ? ' avatar="'.$instance['widget_avatar'].'"' : '';
+				$shortcode .= ']';
+				?>
+				<pre class="mtphr-widgets-code"><p><?php echo $shortcode; ?></p></pre>
+			</span>
+	
+		<?php } // End else statement
+	}
 }
 
 
@@ -292,3 +248,13 @@ function mtphr_display_twitter_widget_feed( $twitter_feed, $widget_limit, $twitt
 
 	echo $output;
 }
+
+
+/* --------------------------------------------------------- */
+/* !Register the widget - 2.2 */
+/* --------------------------------------------------------- */
+
+function mtphr_twitter_widget_init() {
+	register_widget( 'mtphr_twitter_widget' );
+}
+add_action( 'widgets_init', 'mtphr_twitter_widget_init' );
